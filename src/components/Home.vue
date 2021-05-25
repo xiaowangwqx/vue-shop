75656abb2cfee0609f9fbd5fc7d803c3d93381f3
qxww <2669393919@qq.com>
2021-05-25 13:30:30 +0800

    登录功能完成

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>home</h1>
+    <button type="info" @click="logout">退出</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      // 清空token 退回登录页面
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    },
+  },
+}
+</script>
+
+<style lang='less' scoped>
+</style>
